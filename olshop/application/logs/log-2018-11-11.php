ec803eab647bea0d859ffa3c71ccc938441b6f50
galihsatriawan <satriawan.galih99@gmail.com>
2018-11-11 17:24:18 +0700

    Add New Frame of Baby Shark

---
+++ olshop/application/logs/log-2018-11-11.php
@@ -0,0 +1,229 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-11-11 14:12:24 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 14:22:45 --> 404 Page Not Found: ../modules/admin/controllers//index
+ERROR - 2018-11-11 14:22:58 --> 404 Page Not Found: 
+ERROR - 2018-11-11 14:23:01 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 14:23:07 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 14:23:11 --> 404 Page Not Found: 
+ERROR - 2018-11-11 14:33:25 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 14:34:18 --> Severity: Notice --> Undefined index: shop_categorie /var/www/html/olshop/application/modules/vendor/models/Products_model.php 49
+ERROR - 2018-11-11 14:34:18 --> Severity: Notice --> Undefined index: brand_id /var/www/html/olshop/application/modules/vendor/models/Products_model.php 52
+ERROR - 2018-11-11 14:34:18 --> Query error: Column 'shop_categorie' cannot be null - Invalid query: INSERT INTO `products` (`image`, `shop_categorie`, `quantity`, `position`, `brand_id`, `folder`, `vendor_id`, `time`) VALUES ('IMG-20180927-WA0009.jpg', NULL, '9', '1', NULL, '1541921615', '1', 1541921658)
+ERROR - 2018-11-11 15:26:47 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:26:59 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:27:01 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:27:04 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:27:05 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:27:52 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:27:57 --> 404 Page Not Found: ../modules/vendor/controllers//index
+ERROR - 2018-11-11 15:28:01 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 15:28:38 --> Severity: Notice --> Undefined index: brand_id /var/www/html/olshop/application/modules/vendor/models/Products_model.php 52
+ERROR - 2018-11-11 15:28:51 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:28:59 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:29:16 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2018-11-11 15:29:16 --> Severity: Notice --> Undefined index: brand_id /var/www/html/olshop/application/modules/vendor/models/Products_model.php 34
+ERROR - 2018-11-11 15:29:23 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 15:29:31 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 15:29:38 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 15:30:48 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:30:55 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:30:58 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:31:04 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:31:15 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:31:18 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:31:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:31:31 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 15:32:00 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2018-11-11 15:32:05 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:15 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:15 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:15 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:15 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:27 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:28 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:28 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:28 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:32:59 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2018-11-11 15:32:59 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:33:04 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:33:11 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 15:33:42 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:33:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:33:47 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:33:49 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:33:49 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:33:58 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:34:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:34:08 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:16 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:17 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:17 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:17 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:18 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:18 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:18 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:18 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:24 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:31 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:32 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:34 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:35:41 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:36:07 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:36:07 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:36:07 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:36:07 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:36:07 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:36:08 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:15 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:23 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:29 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:43 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:43 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:43 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:43 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:48 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:53 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:53 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:53 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:37:53 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:43:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:44:40 --> 404 Page Not Found: Home/login
+ERROR - 2018-11-11 15:50:04 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:50:14 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:50:14 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:50:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:50:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:50:19 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:51:21 --> Severity: Notice --> Undefined index: logged_user /var/www/html/olshop/application/controllers/Users.php 77
+ERROR - 2018-11-11 15:51:21 --> Severity: Notice --> Undefined index: logged_user /var/www/html/olshop/application/controllers/Users.php 78
+ERROR - 2018-11-11 15:51:21 --> Severity: Notice --> Undefined index: logged_user /var/www/html/olshop/application/controllers/Users.php 79
+ERROR - 2018-11-11 15:53:36 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:54:14 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:54:17 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:54:46 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:55:09 --> Could not find the language line "vendor_home_page"
+ERROR - 2018-11-11 15:55:31 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:55:53 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:56:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:56:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:56:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:56:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:56:12 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 15:56:17 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:50:47 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:51:46 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:51:57 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:56:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:56:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:56:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:56:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:56:51 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:56:58 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:57:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:57:10 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:57:10 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:57:11 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:57:11 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:57:22 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 16:57:25 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:04:23 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:04:23 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:04:24 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:04:25 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:06:32 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:08:12 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:08:22 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:08:22 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:08:22 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:08:22 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:08:29 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:10 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:22 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:26 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:26 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:26 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:40 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:47 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:58 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:10:59 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:02 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:09 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:09 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:09 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:14 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:28 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:28 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:28 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:40 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:40 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:11:40 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:12:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:12:35 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:12:37 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:12:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:13:21 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:13:33 --> Severity: Notice --> Undefined index: logged_user /var/www/html/olshop/application/controllers/Users.php 77
+ERROR - 2018-11-11 17:13:33 --> Severity: Notice --> Undefined index: logged_user /var/www/html/olshop/application/controllers/Users.php 78
+ERROR - 2018-11-11 17:13:33 --> Severity: Notice --> Undefined index: logged_user /var/www/html/olshop/application/controllers/Users.php 79
+ERROR - 2018-11-11 17:13:33 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:13:34 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:13:36 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:13:38 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:13:53 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:13:56 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:13:56 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:14:14 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:14:14 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:14:14 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:14:18 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:14:18 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:14:35 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:14:48 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2018-11-11 17:14:48 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:14:56 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:00 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:01 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:04 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:06 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:06 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:33 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:34 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:35 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:35 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:35 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:35 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:35 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:35 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:38 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:56 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:56 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:57 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:57 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:57 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:57 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:57 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:57 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:15:57 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:15:57 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:16:32 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:16:32 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:16:33 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:16:33 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:16:39 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:16:41 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:16:42 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:16:45 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:16:59 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2018-11-11 17:16:59 --> 404 Page Not Found: /index
+ERROR - 2018-11-11 17:17:11 --> Could not find the language line "user_order_history"
+ERROR - 2018-11-11 17:17:11 --> 404 Page Not Found: /index
