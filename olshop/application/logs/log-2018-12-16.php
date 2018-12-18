1e22acb7b95bde2c688207939ab7a3a3e5e22f6f
nadyarizky <nadyaryota@gmail.com>
2018-12-18 09:10:01 +0700

    Menambahkan bahasa indonesia

---
+++ olshop/application/logs/log-2018-12-16.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-16 16:22:14 --> Severity: error --> Exception: syntax error, unexpected 'Buat' (T_STRING) C:\xampp\htdocs\babyshark-web\olshop\application\language\indonesia\site_lang.php 158
+ERROR - 2018-12-16 16:22:57 --> Severity: error --> Exception: syntax error, unexpected ''Buat Akun Penjual Baru'' (T_CONSTANT_ENCAPSED_STRING) C:\xampp\htdocs\babyshark-web\olshop\application\language\indonesia\site_lang.php 158
+ERROR - 2018-12-16 16:22:59 --> Severity: error --> Exception: syntax error, unexpected ''Buat Akun Penjual Baru'' (T_CONSTANT_ENCAPSED_STRING) C:\xampp\htdocs\babyshark-web\olshop\application\language\indonesia\site_lang.php 158
