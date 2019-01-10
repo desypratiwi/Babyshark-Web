a1ac0204ad78214c621ff0dd4a03c9d62adc20ff
yonescynthia <yonesjuno@gmail.com>
2019-01-10 20:35:29 +0700

    view banner

---
+++ admin/application/views/banner_promo/u_banner.php
@@ -0,0 +1,16 @@
+
+<div class="inner-block">
+<!--KONTEN-->
+<?php echo form_open_multipart('upload/do_upload');?>
+	<input type="file" name="userfile" size="20" />
+
+			<br /><br />
+
+			<input type="submit" value="upload" />
+</form>
+<!--KONTEN-->
+  <div class="clearfix"> </div>
+</div>
+<!--climate start here-->
+<!--climate end here-->
+
