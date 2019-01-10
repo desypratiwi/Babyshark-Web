7d176e48e2826911687654500611f8e45682065b
yonescynthia <yonesjuno@gmail.com>
2019-01-10 21:04:41 +0700

    controller
    menambahkan controller, model

---
+++ admin/application/controllers/c_banner.php
@@ -0,0 +1,27 @@
+<?php 
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	class c_banner extends CI_Controller {
+		public function __construct(){
+			parent::__construct();
+			$this->load->model('m_banner');
+			$this->load->helper('url');
+		}
+		
+		public function index(){
+			
+			$this->load->viewku('banner_promo/u_banner');
+		}
+		
+		public function addBanner(){	
+			$value = array(
+        		'id_banner' => $this->input->post('id'),
+        		'f_banner' => $this->input->post('file'),
+        		'l_banner' => $this->input->post('link')
+        	);
+			$this->m_banner->insertBanner($value);
+			$this->load->view('u_banner');
+		}
+		
+	}
+?>
