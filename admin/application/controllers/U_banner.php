1733aafc7004b10734199cb9ecaa6f2b9ecacc83
rasuyas <rasuyas@gmail.com>
2019-01-16 11:52:20 +0700

    Code Cleanup

--- admin/application/controllers/U_banner.php
+++ admin/application/controllers/U_banner.php
@@ -29,28 +29,12 @@ class U_banner extends CI_Controller{
 			$data = array('upload_data' => $this->upload->data());
 			$data = $this->upload->data();
 			$nama = $data['file_name'];
+			
 			$this->M_banner->databaseName($nama);
-			$this->load->viewku('banner_promo/v_upload_sukses', $data);
+			$this->load->viewku('banner_promo/v_banner', $data);
 			//$data['tb_banner_promo'] = $this->M_banner->view();
 			//$this->load->view('banner_promo/v_banner', $data);
 		}
 	}
  
-	public function add(){
-		$data = array();
-
-		if($this->input->post('submit')){//ketika meekan tombol submit
-			//lakukan upload file dengan memanggil function upload yang ada di model
-			$upload = $this->M_banner->upload();
-
-			if($upload['result'] == "success"){//jika proses upload berhasil maka akan disimpan ke database
-				$this->M_banner->save($upload);
-				redirect('banner_promo/v_banner');//kembali ke halaman awal/view data
-			}
-			else{//jika proses upload gagal
-				$data['message'] = $upload['error'];
-			}
-		}
-		$this->load->view('banner_promo/form', $data);
-	}
 }
--- admin/application/models/m_banner.php
+++ admin/application/models/m_banner.php
@@ -5,38 +5,6 @@
 			return $this->db->get('tb_banner_promo')->result();
 		}
 
-		//fungsi untuk proses upload file
-		public function upload(){
-			$config['upload_path']   = './admin/uploads/';
-			$config['allowed_types'] = 'gif|jpg|png';
-			$config['max_size']      = 100;
-			$config['max_width']     = 1024;
-			$config['max_height']    = 768;
-
-			$this->load->library('upload', $config);
-			if ($this->upload->do_upload('input_banner')){
-
-				//jika berhasil
-				$return = array('result' => 'success', 'file' => $this->upload->data(), 'error' => '');
-				return $return;
-			}
-			else{
-				//jika gagal
-				$return = array('result' => 'failed', 'file' => '', 'error' => $this->upload->display_errors());
-				return $return;
-			}
-
-		}
-		//fungsi menyimpan data ke database
-		public function save($upload){
-			$data = array(
-				'id_banner'=>$upload['file']['id'],
-				'f_banner'=>$upload['file']['nama_file'],
-				'l_banner'=>$upload['file']['link_banner'],
-				'desc'=>$this->input->post('input_desc'));
-			$this->db->insert('tb_banner_promo', $data);
-		}
-
 		public function databaseName($name){
 			$data = array(
 				'id_banner' => '',
