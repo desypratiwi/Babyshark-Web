9723a451683d7fa53225b3e1f23151262c253518
yonescynthia <yonesjuno@gmail.com>
2019-01-17 03:22:32 +0700

    update tabel
    menambahkan deskripsi dan tanggal di database, menghiasi tabel aku ingin terbang dan iyaaaa

--- admin/application/views/banner_promo/v_banner.php
+++ admin/application/views/banner_promo/v_banner.php
@@ -3,11 +3,14 @@
 		<title>Upload Banner</title>
 	</head>
 		<body>
+      <h4>Tambahkan Banner</h4>
+      <br>
 			<?php echo form_open_multipart('U_banner/aksi_upload');?>
-		 
+		
 			<input type="file" name="userfile" size="20" />
+      <input type="text" name="desc" placeholder="Deskripsi" />
 		 
-			<br /><br />
+			<br></br>
 		 
 			<input type="submit" value="upload" />
 		 
@@ -18,20 +21,40 @@
 
 
 <div class="inner-block">
+
 <!--KONTEN-->
-<h1>Data Banner</h1><hr>
-  <a href="<?php echo base_url("index.php/admin/view/banner_promo/form"); ?>">Tambah Banner</a><br><br>
-<table border="1" cellpadding="8">
-<tr>
-  <th>Gambar Banner</th>
-  <th>ID Banner</th>
-  <th>File Banner</th>
-</tr>
+<style>
+table {
+  width:100%;
+}
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;
+}
+th{
+  background-color: lightgrey;
+}
+</style>
+<h1>Data Banner</h1>
+  <table border="1" cellpadding="8">
+    <tr>
+      <th>Gambar Banner</th>
+      <th>ID Banner</th>
+      <th>Nama File</th>
+      <th>Deskripsi</th>
+      <th>Tanggal Upload</th>
+    </tr>
 <?php foreach($table as $row){ // Lakukan looping pada variabel gambar dari controller
     echo "<tr>";
-    echo "<td><img src='".base_url("./gambar/".$row->nama_file)."' width='100' height='100'></td>";
+    echo "<td><img src='".base_url("./gambar/".$row->nama_file)."' width='150' height='100'></td>";
     echo "<td>".$row->id_banner."</td>";
     echo "<td>".$row->nama_file."</td>";
+    echo "<td>".$row->deskripsi."</td>";
+    echo "<td>".$row->tanggal_upload."</td>";
     echo "</tr>";
   }
 ?>
